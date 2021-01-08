cb2d1956416672a877cf27c10b2891a7b9a7903a
matthew.office <matthew.office@accenture.com>
2021-01-08 22:40:14 +0000

    Added JPA annotations to some domain objects

--- src/main/java/com/mdo/tacocloud/Taco.java
+++ src/main/java/com/mdo/tacocloud/Taco.java
@@ -1,6 +1,8 @@
 package com.mdo.tacocloud;
 
 import lombok.Data;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -8,8 +10,11 @@ import java.util.Date;
 import java.util.List;
 
 @Data
+@Entity
 public class Taco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Date createdAt;
@@ -18,7 +23,12 @@ public class Taco {
     @Size(min=5, message="Name must be 5 letters or greater")
     private String name;
 
+    @ManyToMany(targetEntity = Ingredient.class)
     @Size(min=1, message="You must choose at least one ingredient")
     private List<String> ingredients;
 
+    @PrePersist
+    void setCreateAt() {
+        this.createdAt = new Date();
+    }
 }
--- src/main/java/com/mdo/tacocloud/web/DesignTacoController.java
+++ src/main/java/com/mdo/tacocloud/web/DesignTacoController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
